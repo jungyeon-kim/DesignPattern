1acce55680a0d6effecc1bb990aecc1c5ae76acf
Kim Jung Yeon <zoemfhs123@gmail.com>
2022-02-14 19:55:57 +0900

    명령 패턴
    코드일부 작성
    아직 관련지식이 정착되지않아 잠시 보류

--- DesignPattern/Command.cpp
+++ DesignPattern/Command.cpp
@@ -1,1 +1,25 @@
-#include "Command.h"
+#include <iostream>
+
+using namespace std;
+
+/*
+    명령 패턴:  함수를 객체화시켜 사용하는 패턴
+*/
+
+class Command abstract
+{
+    Command() = default;
+    virtual ~Command() = default;
+
+    virtual void Execute() = 0;
+    virtual void Undo() = 0;
+};
+
+class InputController
+{
+    
+};
+
+int main()
+{
+}
