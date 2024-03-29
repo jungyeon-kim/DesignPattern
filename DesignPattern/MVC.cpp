0f93d3114510b47e91abd027ac1353d4289970f4
jungyeon-kim <43169738+jungyeon-kim@users.noreply.github.com>
2024-03-29 14:00:40 +0900

    MVC 패턴
    주석 설명 추가

--- DesignPattern/MVC.cpp
+++ DesignPattern/MVC.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 /*
-    MVC 패턴: Model + View + Controller로 나눠 코드를 관리하는 패턴
+    MVC 패턴: Model + View + Controller로 나눠 코드를 관리하는 패턴 (사용자의 액션은 Controller에서 처리한다.)
 
     장점: 간단하게 데이터와 UI를 나눠 관리할 수 있다.
     단점: Model과 View 사이에 의존성이 높다.
