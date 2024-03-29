658a6e97a57704d68e3af3f3fc9e37f598c18e78
jungyeon-kim <43169738+jungyeon-kim@users.noreply.github.com>
2024-03-29 14:20:13 +0900

    MVC 패턴
    내용 보강

--- DesignPattern/MVC.cpp
+++ DesignPattern/MVC.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 /*
-    MVC 패턴: Model + View + Controller로 나눠 코드를 관리하는 패턴 (사용자의 액션은 Controller에서 처리한다.)
+    MVC 패턴: Model + View + Controller로 나눠 코드를 관리하는 패턴
+
+    순서: UserInput -> Controller -> Model -> Controller -> View
 
     장점: 간단하게 데이터와 UI를 나눠 관리할 수 있다.
     단점: Model과 View 사이에 의존성이 높다.
@@ -28,12 +31,16 @@ class Controller
 {
 private:
     Model Model;
-    View View;
+    vector<View> ViewList;
 public:
     void OnUserScroll(float const InScrollOffset)
     {
         Model.Data = InScrollOffset;
-        View.RefreshUI(Model.Data); // View와 Model의 커플링 발생
+
+        for (View& View : ViewList)
+        {
+            View.RefreshUI(Model.Data); // View와 Model의 커플링 발생
+        }
     }
 };
 
