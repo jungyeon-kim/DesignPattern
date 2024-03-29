31e7ec103db2c796b47eecaaeaea7e0abf376f8d
jungyeon-kim <43169738+jungyeon-kim@users.noreply.github.com>
2024-03-29 14:57:27 +0900

    MVC 패턴
    간단하게 MVP, MVVM 내용 추가

--- DesignPattern/MVC.cpp
+++ DesignPattern/MVC.cpp
@@ -10,6 +10,11 @@ using namespace std;
 
     장점: 간단하게 데이터와 UI를 나눠 관리할 수 있다.
     단점: Model과 View 사이에 의존성이 높다.
+
+    +)
+    단점을 위해 파생된 패턴인 MVP와 MVC가 있다.
+    MVP는 Controller 대신 Presenter라는 개념을 도입해 Model과 View의 중간다리 역할로 Model과 View의 의존성을 없앴다. 하지만 View와 Prensenter 간의 의존성이 생기는게 문제다.
+    MVVM은 Model View ViewModel의 약자로 Presenter대신 View를 위한 Model, ViewModel의 개념을 도입했다.
 */
 
 class Model
