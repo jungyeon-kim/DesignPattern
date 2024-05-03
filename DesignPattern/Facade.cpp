436160111e44aca0185f15a07675a35f36f6a2b5
jungyeon-kim <43169738+jungyeon-kim@users.noreply.github.com>
2024-05-03 18:22:10 +0900

    파사드 패턴
    파사드 패턴 개념 정리

--- DesignPattern/Facade.cpp
+++ DesignPattern/Facade.cpp
@@ -1,1 +1,58 @@
+#include <iostream>
 
+using namespace std;
+
+/*
+    파사드 패턴: 여러 서브 시스템들을 숨기고 하나의 인터페이스로 제공해주는 패턴
+
+    장점: 사용처와 서브 시스템 간의 커플링이 감소한다.
+*/
+
+class SubsystemA
+{
+public:
+    void DoTask()
+    {
+        cout << "SubsystemA" << endl;
+    }
+};
+
+class SubsystemB
+{
+public:
+    void DoTask()
+    {
+        cout << "SubsystemB" << endl;
+    }
+};
+
+class SubsystemC
+{
+public:
+    void DoTask()
+    {
+        cout << "SubsystemC" << endl;
+    }
+};
+
+class Facade
+{
+private:
+    SubsystemA A;
+    SubsystemB B;
+    SubsystemC C;
+public:
+    void DoTask()
+    {
+        A.DoTask();
+        B.DoTask();
+        C.DoTask();
+    }
+};
+
+int main()
+{
+    Facade F;
+
+    F.DoTask();
+}
